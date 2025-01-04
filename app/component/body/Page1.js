0e33f6df7393382261122042aa2f3ca8628dcd6e
imam hossen <imam.frontend404@gmail.com>
2025-01-04 23:24:09 +0600

    flex-wrap-fixing

--- app/component/body/Page1.js
+++ app/component/body/Page1.js
@@ -8,7 +8,7 @@ const Page1 = () => {
     return (
         <div className='border-t-[1px] border-[#DEE0D9] w-full'>
             <div className='container mx-auto'>
-                <div className='flex items-center justify-start gap-[10px] my-[22px ] md:my-[30px] xl:my-[44px]'>
+                <div className='flex items-center justify-start gap-[10px] my-[22px ] md:my-[30px] xl:my-[44px] flex-wrap'>
                     {buttonValues.map((value, index) => (
                         <button
                             key={index}
